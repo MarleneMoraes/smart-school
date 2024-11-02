3e2cf0ce73b0ee36d2ef1a508fff78342ef16e0f
Marlene Moraes <marlenevmoraes@outlook.com>
2024-11-02 18:45:07 -0300

    Student Model Class

--- SmartSchool.WebAPI/Models/Student.cs
+++ SmartSchool.WebAPI/Models/Student.cs
@@ -7,6 +7,18 @@ namespace SmartSchool.WebAPI.Models
 {
     public class Student
     {
+        public Student() { }
+        public Student(int id, string name, string lastName, string telephone)
+        {
+            this.Id = id;
+            this.Name = name;
+            this.LastName = lastName;
+            this.Telephone = telephone;
+        }
         
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string Telephone { get; set; }
     }
 }
