ff2be1fd61ed82ce41b00e28c25f8c2180311df5
Marlene Moraes <marlenevmoraes@outlook.com>
2024-11-02 18:45:43 -0300

    Subject Model Class

--- SmartSchool.WebAPI/Models/Subject.cs
+++ SmartSchool.WebAPI/Models/Subject.cs
@@ -7,6 +7,19 @@ namespace SmartSchool.WebAPI.Models
 {
     public class Subject
     {
-        
+        public Subject() { }
+
+        public Subject(int id, string name, int teacherId)
+        {
+            this.Id = id;
+            this.Name = name;
+            this.TeacherId = teacherId;
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int TeacherId { get; set; }
+        public Teacher Teacher { get; set; }
+
     }
 }
