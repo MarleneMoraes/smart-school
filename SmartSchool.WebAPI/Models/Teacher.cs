c734e8e373e01fa45b9134d8dce8002e2c460940
Marlene Moraes <marlenevmoraes@outlook.com>
2024-11-02 18:52:34 -0300

    Teacher Model Class

--- SmartSchool.WebAPI/Models/Teacher.cs
+++ SmartSchool.WebAPI/Models/Teacher.cs
@@ -7,6 +7,14 @@ namespace SmartSchool.WebAPI.Models
 {
     public class Teacher
     {
-        
+        public Teacher() { }
+        public Teacher(int id, string name)
+        {
+            this.Id = id;
+            this.Name = name;
+        }
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public IEnumerable<Subject> Subjects { get; set; }
     }
 }
