e894bd36306addcd7f11adbe4b8aa1d8bd941740
Marlene Moraes <marlenevmoraes@outlook.com>
2024-11-02 22:32:02 -0300

    Refactoring GET and create GET by Id and by Name Student

--- SmartSchool.WebAPI/Controllers/StudentController.cs
+++ SmartSchool.WebAPI/Controllers/StudentController.cs
@@ -1,8 +1,6 @@
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using SmartSchool.WebAPI.Models;
 
 namespace SmartSchool.WebAPI.Controllers
 {
@@ -10,12 +8,49 @@ namespace SmartSchool.WebAPI.Controllers
     [Route("api/[controller]")]
     public class StudentController : ControllerBase
     {
+        public List<Student> Students = new List<Student>()
+        {
+            new Student() { 
+                Id = 1, 
+                Name = "Marcos", 
+                LastName = "Almeida",
+                Telephone = "91234-5678" 
+            },
+            new Student() { 
+                Id = 2, 
+                Name = "Marta", 
+                LastName = "Kent",
+                Telephone = "98765-4321" 
+            },
+            new Student() { 
+                Id = 3, 
+                Name = "Laura", 
+                LastName = "Maria",
+                Telephone = "91234-8765" 
+            }
+        };
         public StudentController() { }
 
         [HttpGet]
         public IActionResult Get()
         {
-            return Ok("Students: Anderson, Felipe, Ana, Luana");
-        }   
+            return Ok(Students);
+        }
+
+        [HttpGet("{id:int}")]
+        public IActionResult GetById(int id)
+        {
+            var student = Students.FirstOrDefault(a => a.Id == id);
+            if (student == null) return BadRequest("Student not found");
+            return Ok(student);
+        }
+
+        [HttpGet("{name}")]
+        public IActionResult GetByName(string name, string lastName)
+        {
+            var student = Students.FirstOrDefault(a => a.Name == name && a.LastName == lastName);
+            if (student == null) return BadRequest("Student not found");
+            return Ok(student);
+        }
     }
 }
